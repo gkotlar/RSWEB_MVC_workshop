518a63a51f62d6212398617d8a27d67cf122d245
gkotlar <gjorgji.kotlarovski@gmail.com>
2024-05-19 15:08:19 +0200

    changes to seeddata

--- BookstoreApp/Models/SeedData.cs
+++ BookstoreApp/Models/SeedData.cs
@@ -1,7 +1,10 @@
 ﻿using BookstoreApp.Areas.Identity.Data;
+using BookstoreApp.Migrations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using System.IO;
+using System.Security.Policy;
 
 
 namespace BookstoreApp.Models;
@@ -127,101 +130,101 @@ public class SeedData
             context.Book.AddRange(
                 new Book 
                 { /*Id = 1, */ 
-                    Title = "Song Birds Tale", 
-                    YearPublished = 2012, 
-                    NumPages = 410, 
-                    Description = "Lacking", 
-                    Publisher = "Store House Media",
-                    FrontPage = "",
-                    DownloadUrl = "",
+                    Title = "Music and manners in France and Germany : a series of travelling sketches of art and society", 
+                    YearPublished = 1768, 
+                    NumPages = 328, 
+                    Description = "When mysterious letters start arriving on his doorstep, Harry Potter has never heard of Hogwarts School of Witchcraft and Wizardry. They are swiftly confiscated by his aunt and uncle. Then, on Harry’s eleventh birthday, a strange man bursts in with some important news: Harry Potter is a wizard and has been awarded a place to study at Hogwarts. And so the first of the Harry Potter adventures is set to begin.", 
+                    Publisher = "Media Rodzina",
+                    FrontPage = "/BookCoverPages/musicmannersinfr02chor_0001.jpg",
+                    DownloadUrl = "/BookCoverPages/musicmannersinfr02chor_0001.jpg",
                     AuthorId = 1 
                 },
                 new Book
                 {/*Id = 2, */
-                    Title = "quis diam. Pellentesque",
-                    YearPublished = 2022,
-                    NumPages = 650,
-                    Description = "mauris. Suspendisse aliquet molestie tellus. Aenean egestas hendrerit neque. In ornare sagittis felis. Donec tempor, est ac mattis semper, dui lectus rutrum urna, nec luctus felis purus ac tellus. Suspendisse sed dolor. Fusce mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus. Donec nibh enim, gravida sit amet, dapibus id, blandit at, nisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel nisl. Quisque fringilla euismod enim. Etiam gravida molestie arcu. Sed eu nibh vulputate mauris sagittis placerat.",
+                    Title = "Wuthering Heights",
+                    YearPublished = 2020,
+                    NumPages = 104,
+                    Description = "Wuthering Heights is an 1847 novel by Emily Brontë, initially published under the pseudonym Ellis Bell.It concerns two families of the landed gentry living on the West Yorkshire moors the Earnshaws and the and their turbulent relationships with Earnshaw's adopted son, Heathcliff. The novel was influenced by Romanticism and Gothic fiction.",
                     Publisher = "In Tempus Corporation",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0014543388-L.jpg",
+                    DownloadUrl = "/BookCoverPages/0014543388-L.jpg",
                     AuthorId = 1
                 },
                 new Book
                 {/*Id = 3, */
-                    Title = "nunc.",
-                    YearPublished = 2015,
-                    NumPages = 161,
-                    Description = "vulputate mauris sagittis placerat. Cras dictum ultricies ligula. Nullam enim. Sed nulla ante, iaculis nec, eleifend non, dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero. Proin sed turpis nec mauris blandit mattis. Cras eget nisi dictum augue malesuada malesuada. Integer id magna et ipsum cursus vestibulum. Mauris magna. Duis dignissim tempor arcu. Vestibulum ut eros non enim commodo hendrerit. Donec porttitor tellus non magna. Nam ligula elit, pretium et, rutrum non, hendrerit id, ante. Nunc mauris sapien, cursus in, hendrerit consectetuer, cursus et, magna. Praesent interdum ligula eu enim. Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam suscipit, est ac",
+                    Title = "Les quatre filles du docteur March",
+                    YearPublished = 2004,
+                    NumPages = 344,
+                    Description = "",
                     Publisher = "Mi Ac Corp.",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0012143522-L.jpg",
+                    DownloadUrl = "/BookCoverPages/0012143522-L.jpg",
                     AuthorId = 2
                 },
                 new Book
                 {/*Id = 4, */
-                    Title = "feugiat metus sit",
+                    Title = "Little Women",
                     YearPublished = 2012,
                     NumPages = 173,
                     Description = "Sed eu eros. Nam consequat dolor vitae dolor. Donec fringilla. Donec feugiat metus sit amet ante. Vivamus non lorem vitae odio sagittis semper. Nam tempor diam dictum sapien. Aenean massa. Integer vitae nibh. Donec est mauris, rhoncus id, mollis nec, cursus a, enim. Suspendisse aliquet, sem ut cursus luctus, ipsum leo elementum sem, vitae aliquam eros turpis non enim. Mauris quis turpis vitae purus gravida sagittis. Duis gravida. Praesent eu nulla at sem molestie sodales. Mauris blandit enim consequat purus. Maecenas libero est, congue a, aliquet vel, vulputate eu, odio. Phasellus at augue id ante dictum cursus. Nunc mauris elit, dictum eu, eleifend nec, malesuada ut, sem. Nulla interdum. Curabitur dictum. Phasellus in felis. Nulla tempor augue ac ipsum.",
                     Publisher = "Gravida Sagittis Corp.",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0012812507-L.jpg",
+                    DownloadUrl = "/BookCoverPages/0012812507-L.jpg",
                     AuthorId = 6
                 },
                 new Book
                 {/*Id = 5, */
-                    Title = "ipsum. Suspendisse sagittis. Nullam",
+                    Title = "Charlie and the Chocolate Factory",
                     YearPublished = 2016,
                     NumPages = 154,
-                    Description = "id risus quis diam luctus lobortis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Mauris ut quam vel sapien imperdiet ornare. In faucibus. Morbi vehicula. Pellentesque tincidunt tempus risus. Donec egestas. Duis ac arcu. Nunc mauris. Morbi non sapien molestie orci tincidunt adipiscing. Mauris molestie pharetra nibh. Aliquam ornare, libero at auctor ullamcorper, nisl arcu iaculis enim, sit amet ornare lectus justo eu arcu. Morbi sit amet massa. Quisque porttitor eros nec tellus. Nunc lectus pede, ultrices a, auctor non, feugiat nec, diam. Duis mi enim, condimentum",
+                    Description = "'I, WILLY WONKA, have DECIDED to allow FIVE CHILDREN to visit my FACTORY this year. These LUCKY FIVE will be allowed to see all THE SECRETS AND MAGIC.'\r\n--back cover",
                     Publisher = "In Corporation",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0010654862-L.jpg",
+                    DownloadUrl = "/BookCoverPages/0010654862-L.jpg",
                     AuthorId = 8
                 },
                 new Book
                 {/*Id = 6, */
-                    Title = "dui augue",
-                    YearPublished = 2013,
+                    Title = "Teias de charlotte",
+                    YearPublished = 2002,
                     NumPages = 205,
-                    Description = "nisi dictum augue malesuada malesuada. Integer id magna et ipsum cursus vestibulum. Mauris magna. Duis dignissim tempor arcu. Vestibulum ut eros non enim commodo hendrerit. Donec porttitor tellus non magna. Nam ligula elit, pretium et, rutrum non, hendrerit id, ante. Nunc mauris sapien, cursus in, hendrerit consectetuer, cursus et, magna. Praesent interdum ligula eu enim. Etiam imperdiet dictum magna. Ut tincidunt orci quis lectus. Nullam suscipit, est ac facilisis facilisis, magna tellus faucibus leo, in lobortis tellus justo sit amet nulla. Donec non justo. Proin non massa non ante bibendum ullamcorper. Duis cursus, diam at pretium aliquet, metus urna convallis erat, eget tincidunt dui augue eu tellus. Phasellus elit pede, malesuada vel, venenatis vel, faucibus id, libero. Donec consectetuer mauris id sapien. Cras dolor dolor, tempus non, lacinia at, iaculis quis, pede. Praesent eu dui. Cum sociis",
+                    Description = "Wilbur the pig is desolate when he discovers that he is destined to be the farmer's Christmas dinner until his spider friend, Charlotte, decides to help him.\r\n\r\n",
                     Publisher = "Morbi Institute",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0008311468-L.jpg",
+                    DownloadUrl = "/BookCoverPages/0008311468-L.jpg",
                     AuthorId = 2
                 },
                 new Book
                 {/*Id = 7, */
-                    Title = "Donec vitae erat vel",
-                    YearPublished = 2015,
+                    Title = "1984",
+                    YearPublished = 2013,
                     NumPages = 560,
-                    Description = "sed, facilisis vitae, orci. Phasellus dapibus quam quis diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce aliquet magna a neque. Nullam ut nisi a odio semper cursus. Integer mollis. Integer tincidunt aliquam arcu. Aliquam ultrices iaculis odio. Nam interdum enim non nisi. Aenean eget metus. In nec orci. Donec nibh. Quisque nonummy ipsum non arcu. Vivamus sit amet risus. Donec egestas. Aliquam nec enim. Nunc ut erat. Sed nunc est, mollis non, cursus non, egestas a, dui. Cras pellentesque.",
+                    Description = "Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can't escape the fact that Big Brother is always watching...\r\n\r\n",
                     Publisher = "Volutpat Nunc LLP",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0012525678-L.jpg",
+                    DownloadUrl = "/BookCoverPages/0012525678-L.jpg",
                     AuthorId = 5
                 },
                 new Book
                 {/*Id = 8, */
-                    Title = "facilisis eget, ipsum.",
-                    YearPublished = 2017,
+                    Title = "Alice's Adventures in Wonderland\r\n",
+                    YearPublished = 2011,
                     NumPages = 102,
-                    Description = "et, euismod et, commodo at, libero. Morbi accumsan laoreet ipsum. Curabitur consequat, lectus sit amet luctus vulputate, nisi sem semper erat, in consectetuer ipsum nunc id enim. Curabitur massa. Vestibulum accumsan neque et nunc. Quisque ornare tortor at risus. Nunc ac sem ut dolor dapibus gravida. Aliquam tincidunt, nunc ac mattis ornare, lectus ante dictum mi, ac mattis velit justo nec ante. Maecenas mi felis, adipiscing fringilla, porttitor vulputate, posuere vulputate, lacus. Cras interdum. Nunc sollicitudin commodo ipsum. Suspendisse non leo. Vivamus nibh dolor, nonummy ac, feugiat non, lobortis quis, pede. Suspendisse dui.",
+                    Description = "Alice's Adventures in Wonderland (commonly Alice in Wonderland) is an 1865 English children's novel by Lewis Carroll. A young girl named Alice falls through a rabbit hole into a fantasy world of anthropomorphic creatures. It is seen as an example of the literary nonsense genre. One of the best-known works of Victorian literature, its narrative, structure, characters and imagery have had huge influence on popular culture and literature, especially in the fantasy genre.",
                     Publisher = "Cras Convallis Institute",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0014547634-L.jpg",
+                    DownloadUrl = "/BookCoverPages/0014547634-L.jpg",
                     AuthorId = 2
                 },
                 new Book
-                {/*Id = 9, */
+                {/*Id = 9, */   
                     Title = "ullamcorper.",
                     YearPublished = 2020,
                     NumPages = 466,
                     Description = "pede sagittis augue, eu tempor erat neque non quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam fringilla cursus purus. Nullam scelerisque neque sed sem egestas blandit. Nam nulla magna, malesuada vel, convallis in, cursus et, eros. Proin ultrices. Duis volutpat nunc sit amet metus. Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt nibh. Phasellus nulla. Integer vulputate, risus a ultricies adipiscing, enim mi tempor lorem, eget mollis lectus pede et risus. Quisque libero lacus, varius et, euismod et, commodo at, libero. Morbi accumsan laoreet ipsum. Curabitur consequat, lectus sit amet luctus vulputate, nisi sem semper erat, in consectetuer ipsum nunc id enim. Curabitur massa. Vestibulum accumsan neque et nunc. Quisque ornare tortor at risus. Nunc ac sem ut dolor dapibus gravida. Aliquam tincidunt, nunc ac mattis ornare, lectus ante dictum mi, ac mattis velit justo nec ante. Maecenas mi felis, adipiscing fringilla, porttitor vulputate, posuere vulputate, lacus. Cras interdum. Nunc sollicitudin commodo ipsum. Suspendisse non leo. Vivamus nibh dolor, nonummy ac, feugiat non, lobortis quis, pede. Suspendisse dui. Fusce diam nunc, ullamcorper eu, euismod ac, fermentum vel, mauris. Integer sem elit, pharetra ut, pharetra sed,",
                     Publisher = "Mauris Eu Turpis Foundation",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0010527843-M.jpg",
+                    DownloadUrl = "/BookCoverPages/0010527843-M.jpg",
                     AuthorId = 3
                 },
                 new Book
@@ -231,8 +234,8 @@ public class SeedData
                     NumPages = 451,
                     Description = "neque venenatis lacus. Etiam bibendum fermentum metus. Aenean sed pede nec ante blandit viverra. Donec tempus, lorem fringilla ornare placerat, orci lacus vestibulum lorem, sit amet ultricies sem magna nec quam. Curabitur vel lectus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec dignissim magna a tortor. Nunc commodo auctor velit. Aliquam nisl. Nulla eu neque pellentesque massa lobortis ultrices. Vivamus rhoncus. Donec est. Nunc ullamcorper, velit in aliquet lobortis, nisi nibh lacinia orci, consectetuer euismod est arcu ac orci. Ut semper pretium neque. Morbi quis urna. Nunc quis arcu vel quam dignissim pharetra. Nam ac nulla. In tincidunt congue turpis. In condimentum. Donec at arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae Donec tincidunt. Donec vitae erat vel pede blandit congue. In scelerisque scelerisque dui. Suspendisse ac metus vitae velit egestas lacinia. Sed congue, elit",
                     Publisher = "Faucibus Orci Luctus Company",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0008595966-M.jpg",
+                    DownloadUrl = "/BookCoverPages/0008595966-M.jpg",
                     AuthorId = 3
                 },
                 new Book
@@ -242,8 +245,8 @@ public class SeedData
                     NumPages = 481,
                     Description = "sed dolor. Fusce mi lorem, vehicula et, rutrum eu, ultrices sit amet, risus. Donec nibh enim, gravida sit amet, dapibus id, blandit at, nisi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel nisl. Quisque fringilla euismod enim. Etiam gravida molestie arcu. Sed eu nibh vulputate mauris sagittis placerat. Cras dictum ultricies ligula. Nullam enim. Sed nulla ante, iaculis nec, eleifend non, dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed libero. Proin sed turpis nec mauris blandit mattis. Cras eget nisi dictum augue malesuada malesuada. Integer id magna et ipsum cursus vestibulum. Mauris magna. Duis dignissim tempor arcu. Vestibulum ut eros non enim commodo hendrerit. Donec porttitor tellus non magna. Nam",
                     Publisher = "Consectetuer Ipsum Nunc Corp.",
-                    FrontPage = "/",
-                    DownloadUrl = "/",
+                    FrontPage = "/BookCoverPages/0012726168-M.jpg",
+                    DownloadUrl = "/BookCoverPages/0012726168-M.jpg",
                     AuthorId = 1
                 }
             );
@@ -290,7 +293,7 @@ public class SeedData
                 {/*Id = 1 */  
                     BookId = 1, 
                     AppUser = "",
-                    Comment = "",
+                    Comment = "Very good book",
                     Rating = 2
                 },
                 new Review
@@ -367,9 +370,8 @@ public class SeedData
             context.SaveChanges();
 
             context.UserBooks.AddRange(
-                new UserBooks { AppUser = "", BookId = 1}
-
                 );
+
             context.SaveChanges();
         }
     }
